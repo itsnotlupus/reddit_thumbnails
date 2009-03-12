c9c6fe360a5a90cc86a9409ab41743e3222c6cca
itsnotlupus <metalbot@yahoo.com>
2009-03-12 11:09:26 -0500

    minor tweaks, a couple more defines, and some timestamps in the output, to keep track of things

--- Main.php
+++ Main.php
@@ -2,25 +2,31 @@
 <?php
 
 define( "COOKIES", trim(file_get_contents("cookies")));
-define( "SUB",  "cfabbro");
-define( "MAX_WIDTH", 100);
-define( "MAX_HEIGHT", 100);
+define( "SUB",  "reddithax");
+define( "MAX_WIDTH", 180);
+define( "MAX_HEIGHT", 180);
 define( "BUCKETS", 25);
 define( "BUCKET_SIZE", 490000); // a bit under 500K for overhead and such
 
-define( "BGCOLOR", "feefee");
-define( "MARGIN", 15); // vertical pixels to keep transparent between bitmaps.
+define( "FORCED_REFRESH_INTERVAL", 600); // how often to check a story if its comment count hasn't changed
+
+define( "BGCOLOR", "ffffff"); // "dcd4b5"); //"feefee");
+define( "MARGIN", 0); // 12); // vertical pixels to keep transparent between bitmaps.
+define( "NEGMARGIN", 12); // if the MARGIN is 0, you probably want this to be 12.
 
 function __autoload($class) {
   require_once "$class.php";
 }
 
+$time_start = time();
+echo "# reddit_thumbnails::".SUB." started at ".date(DATE_RFC822,$time_start)."\n";
+
 $reddit = new Reddit(SUB, COOKIES);
 $resolver = new URLResolver("images");
 $thumbnailer = new Thumbnailer(MAX_WIDTH, MAX_HEIGHT, "thumbnails", $resolver);
-$scanner = new Scanner($reddit, $thumbnailer);
+$scanner = new Scanner($reddit, $thumbnailer, FORCED_REFRESH_INTERVAL);
 $shuffler = new Shuffler(BUCKETS, BUCKET_SIZE, BGCOLOR, MARGIN);
-$updater = new Updater($reddit);
+$updater = new Updater($reddit, NEGMARGIN);
 
 $scanned  = $scanner->scan(); // take a snapshot of reddit and of its thumbnails
 
@@ -28,3 +34,4 @@ $shuffled = $shuffler->shuffle($scanned); // compose thumbnails into images
 
 $updater->update($shuffled); // post stuff back to the reddit servers
 
+echo "# reddit_thumbnails::".SUB." done in ".(time()-$time_start)." seconds.\n";
